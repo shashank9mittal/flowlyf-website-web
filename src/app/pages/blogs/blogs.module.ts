656297a16294f2c61494da953482974a226b7977
mittalDesign <shashank.mittal@westernacher.com>
2021-02-26 16:15:55 +0530

    generated deployables

--- src/app/pages/blogs/blogs.module.ts
+++ src/app/pages/blogs/blogs.module.ts
@@ -5,7 +5,7 @@ import { BlogsRoutingModule } from './blogs-routing.module';
 import { BlogsComponent } from './blogs.component';
 import { BlogsDetailComponent } from './blogs-detail/blogs-detail.component';
 import { BlogsSubsComponent } from './blogs-subs/blogs-subs.component';
-import {SafeHtmlPipe} from '../../shared/pipes/safe-html.pipe';
+import {SafeHtmlPipe} from './pipes/safe-html.pipe';
 
 
 @NgModule({
